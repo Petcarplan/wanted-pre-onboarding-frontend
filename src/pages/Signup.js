218cefe2493cb373aadddcba0e02ab44d937f397
cheonchanyoung <petcarplan@gmail.com>
2023-06-17 16:09:09 +0900

    로그인, 회원가입 틀 1차 구현

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,3 +1,58 @@
-export default function Signup(){
-    return <>Signup</>
-    }
+import { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+export default function Signup() {
+  const [user, setUser] = useState();
+  const [pw, setPw] = useState();
+  const [signedup, setSignedup] = useState(false);
+
+  const apiAddress = `https://www.pre-onboarding-selection-task.shop/auth/signup`;
+  const navigate = useNavigate();
+
+  const successSignup = () => {
+    navigate('/signin');
+  };
+
+  const callApi = () => {
+    axios
+      .post(
+        apiAddress,
+        {
+          email: user,
+          password: pw,
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      )
+      .then(res => {
+        console.log(res);
+        setSignedup(true);
+      })
+      .catch(err => {
+        console.log(err);
+        alert('회원가입 실패');
+      });
+  };
+
+  return signedup ? (
+    successSignup()
+  ) : (
+    <>
+      <input
+        data-testid="email-input"
+        onChange={e => setUser(e.target.value)}
+      />
+      <input
+        data-testid="password-input"
+        onChange={e => setPw(e.target.value)}
+      />
+      <button data-testid="signup-button" onClick={callApi}>
+        회원가입
+      </button>
+    </>
+  );
+}
